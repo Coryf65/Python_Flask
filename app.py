da1e4032808e5f490260b882271b5384653a98a5
Coryf65 <coryf65@gmail.com>
2020-02-27 15:28:29 -0600

    passed in param as get request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # This file creates the flask app
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 def home():    
     # we are going to user our new template
-    return render_template('home.html', name='Cory')
+    return render_template('home.html')
 
 # adding another route
-@app.route('/about')
+@app.route('/your-url')
 
 # function names and routes don't HAVE to match
-def about():
-    return 'This is a URL shortener'
+def your_url():
+    return render_template('your_url.html', code=request.args['code'])
 
