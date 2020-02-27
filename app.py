7ca2a1bf3e36f09912fbc5b9ed802299ae413d4c
Coryf65 <coryf65@gmail.com>
2020-02-27 15:45:42 -0600

    creating a json file to store urls

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # This file creates the flask app
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -17,6 +18,16 @@ def home():
 def your_url():
 
     if request.method == 'POST':
+
+        # creating a Dictionary to store info
+        urls = {}
+        # specifying the KEY/Name = code, and Value pair
+        urls[request.form['code']] = {'url':request.form['url']}
+
+        # saving to JSON file
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+
         return render_template('your_url.html', code=request.form['code'])
     else:        
         return redirect(url_for('home'))
