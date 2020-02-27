9ae06cdd27b527d9834d4b47f8506186db162425
Coryf65 <coryf65@gmail.com>
2020-02-27 15:33:04 -0600

    telling flask to use a form and a POST and GET for requests

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ def home():
     # we are going to user our new template
     return render_template('home.html')
 
-# adding another route
-@app.route('/your-url')
+# by default flask uses GET requests
+# now we are telling flask to use a POST
+@app.route('/your-url', methods=['GET', 'POST'])
 
 # function names and routes don't HAVE to match
 def your_url():
-    return render_template('your_url.html', code=request.args['code'])
+
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'This is not Valid'
 
