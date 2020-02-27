0ad7293c860bfb245e52840cc473f215dc5fa57f
Coryf65 <coryf65@gmail.com>
2020-02-27 15:48:43 -0600

    creating a json file to store links !

--- app.py
+++ app.py
@@ -10,15 +10,10 @@ def home():
     # we are going to user our new template
     return render_template('home.html')
 
-# by default flask uses GET requests
-# now we are telling flask to use a POST
 @app.route('/your-url', methods=['GET', 'POST'])
-
-# function names and routes don't HAVE to match
 def your_url():
 
     if request.method == 'POST':
-
         # creating a Dictionary to store info
         urls = {}
         # specifying the KEY/Name = code, and Value pair
@@ -27,7 +22,6 @@ def your_url():
         # saving to JSON file
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
-
         return render_template('your_url.html', code=request.form['code'])
     else:        
         return redirect(url_for('home'))
