975bc0da86fabea65d638d61e4d8cc292348170f
Coryf65 <coryf65@gmail.com>
2020-02-27 15:41:09 -0600

    now redirect to the home function in case of page name changes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # This file creates the flask app
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 # creating a route
 @app.route('/')
-
 def home():    
     # we are going to user our new template
     return render_template('home.html')
@@ -19,10 +18,6 @@ def your_url():
 
     if request.method == 'POST':
         return render_template('your_url.html', code=request.form['code'])
-    else:
-        # doing this we could get to the home page, but still see different text in the url
-        #return render_template('home.html')
-
-        #so instead we will do a simple redirect
-        return redirect('/')
+    else:        
+        return redirect(url_for('home'))
 
