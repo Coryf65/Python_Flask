d1387e9a5c631f59df35aaa3a03d6790e5285b29
Coryf65 <coryf65@gmail.com>
2020-02-29 11:10:53 -0600

    setting up pytest

---
+++ conftest.py
@@ -0,0 +1,15 @@
+# setup the app
+
+import pytest 
+from urlshort import create_app
+
+@pytest.fixture
+
+def app():
+    app = create_app()
+    yield app
+
+@pytest.fixture
+
+def client(app):
+    return app.test_client()
